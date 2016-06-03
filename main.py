cb00b48bd84998d3f8ac98180210ddb977ac5d1e
Felix <ident_green@posteo.de>
2016-06-03 23:37:41 +0200

    first code in main.py. waiting for cell.py

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 import time
 t_start=time.time()
-print "hello Lea, hello world."
-print "hello Felix, hello world."
+import cell
+
+# --- methods ---------------------------------------
+
+def initialize_cells(width,length):
+	pass
+	# TODO!!!!!!!!
+	area=[[cell.cell(pos=(x,y)) for x in range(width)] for y in range(length)]
+	return area
+
+def initialize_neighbors(area):
+	# TODO!!!!!!!!
+	pass
+
+def update(area):
+	for y in range(len(area)):
+		for x in range(len(area[0])):
+			area[x][y].update()
+
+def output():
+	pass
+
+# --- variables -------------------------------------
+
+width=10
+length=10
+time_steps=10
+
+# --- main process -----------------------------------
+
+area=initialize_cells(width,length)
+initialize_neighbors(area)
+
+for t in range(time_steps):
+	update(area)
+
+output()
 
 
 
