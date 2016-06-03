344fc78b48e1ad0aabce5632413f74bca7d55032
Felix <ident_green@posteo.de>
2016-06-03 16:48:16 +0200

    added time measurement2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 t_start=time.time()
 
 print "hello Lea, hello world."
-
-print "program finished. ("+str((time.time()-t_start))+")"
-
 print "hello Felix, hello world."
+
+print "program finished. ("+str(int((time.time()-t_start)*1000))+" ms)"
+
