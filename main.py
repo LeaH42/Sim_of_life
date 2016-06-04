8af0facbb6d4c8537a609e0a0c5a69363746840d
Felix <ident_green@posteo.de>
2016-06-04 10:41:08 +0200

    basics for main.py. waiting for cell.py

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import time
 t_start=time.time()
 import cell
 
+# --- variables -------------------------------------
+
+dim=(10,10)
+time_steps=10
+
 # --- methods ---------------------------------------
 
 def initialize_cells(width,length):
 	pass
-	# TODO!!!!!!!!
+	# we need to decide whether position will be given to cell or not
 	area=[[cell.cell(pos=(x,y)) for x in range(width)] for y in range(length)]
 	return area
 
 def initialize_neighbors(area):
-	# TODO!!!!!!!!
-	pass
+	for y in range(len(area)):
+		for x in range(len(area[0])):
+			for i in range(8):
+				coo=[	(x-1,y-1),(x,y-1),(x+1,y-1),	# 012 
+						(x-1,y  ),        (x+1,y  ),	# 3 4
+						(x-1,y+1),(x,y+1),(x+1,y+1)]	# 567
+				pos_x=coo[i][0]%width
+				pos_y=coo[i][1]%length
+				area[x][y].neighbors[i]=area[pos_x][pos_y]
 
 def update(area):
 	for y in range(len(area)):
@@ -20,17 +32,17 @@ def update(area):
 			area[x][y].update()
 
 def output():
-	pass
-
-# --- variables -------------------------------------
-
-width=10
-length=10
-time_steps=10
-
+	for y in range(len(area)):
+		out=" "
+		for x in range(len(area[0])):
+			if area[x][y].get_state():
+				out+="X"
+			else:
+				out+="L"
+		print out
 # --- main process -----------------------------------
 
-area=initialize_cells(width,length)
+area=initialize_cells(dim[0],dim[1])
 initialize_neighbors(area)
 
 for t in range(time_steps):
