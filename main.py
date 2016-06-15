617973c17a7c7c6a46eafc71cb4ea29ed55eb13b
LeaH42 <heinze.lea@gmx.de>
2016-06-15 22:43:51 +0200

    better plot

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import copy
 
 # --- variables -------------------------------------
 
-dim=(10,10)
-time_steps=5
+dim=(200,200)
+time_steps=50
 
 # --- methods ---------------------------------------
 
@@ -71,28 +71,12 @@ def plotvecs(areas,dim):
     # figure initialization
     fig = plt.figure()
     ax = plt.axes(xlim=(0, dim[0]), ylim=(0, dim[1]))
-
-    lines = [ plt.plot([], [], 'sk', ms=10)[0]]
-
-    # empty lines
-    def init():    
-        for line in lines:
-            line.set_data([], [])
-        return lines
     
     #lines[] gets x and y list
     def animate(i): 
-	xdata=[]
-	ydata=[]
-	for y in range(len(areas[i])):
-		for x in range(len(areas[i][0])):
-			if areas[i][x][y]:
-				xdata.append(x)
-				ydata.append(y)
-        lines[0].set_data( xdata, ydata )
-        return lines
+        ax.imshow(areas[i], cmap='Greys', interpolation='none')
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=6, interval=500, blit=False,repeat=False)
+    anim = animation.FuncAnimation(fig, animate, frames=time_steps+1, interval=100, blit=False,repeat=False)
 
     plt.xlim(-1, len(area[0])+1)
     plt.ylim(-1, len(area)+1)
@@ -101,11 +85,11 @@ def plotvecs(areas,dim):
 
 
 def convert_area(area):
-	states=[[False for x in range(len(area[0]))] for y in range(len(area))]
+	states=[[0 for x in range(len(area[0]))] for y in range(len(area))]
 	for y in range(len(area)):
 		for x in range(len(area[0])):
 			if area[x][y].get_state():
-				states[x][y]=True
+				states[x][y]=1
 	return states
 	
 
@@ -119,7 +103,7 @@ areas.append(convert_area(area))
 for t in range(time_steps):
 	update(area)
 	areas.append(convert_area(area))
-	time.sleep(0.5) #why?
+	#time.sleep(0.5) #why?
 
 plotvecs(areas, dim)
 
